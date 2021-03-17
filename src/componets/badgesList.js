800726d40ea8cd3ed7754ed5dfd781ecef22af91
arantza-yes <yeseniaarantza@gmail.com>
2021-03-16 19:48:37 -0500

    agregando filter de busqueda

--- src/componets/badgesList.js
+++ src/componets/badgesList.js
@@ -3,22 +3,72 @@ import { Link } from "react-router-dom";
 import Gravatar from "./Gravatar";
 import "./styles/style.css";
 
-class Badgelist extends React.Component {
-  render() {
-    if (this.props.Badges.length === 0) {
-      return (
-        <div>
-          <h3>no hay datos aun</h3>
-          <Link to="/badges/new" className="badges__button">
-            Create New Badges
-          </Link>
-        </div>
-      );
-    }
+// EMPAQUETANDO
 
+function useSearchBadges(Badges) {
+  // HACIENDO CUSTOM HOOKS
+  const [query, setQuery] = React.useState("");
+
+  const [filteredBadges, setFilteredBadges] = React.useState(Badges);
+
+  // PARA FILTRAR EL INPUT BUSCANDO NOMBRES se cambio por useMemo
+  React.useMemo(() => {
+    const result = Badges.filter((badge) => {
+      return `${badge.firstName} ${badge.lasstName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(result);
+  }, [Badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+// PASAMOS ESTE COMPONENTE DE CLASES A FUNCIONAL PARA USAR HOOKS Y CUSTOMHOOKS
+
+function Badgelist(props) {
+  const Badges = props.Badges;
+
+  const { query, setQuery, filteredBadges } = useSearchBadges(Badges);
+
+  if (filteredBadges.length === 0) {
     return (
+      <div>
+        <div className="form-group">
+          <label className="form-group__label">Filter Badges</label>
+          <input
+            className="form-group__input"
+            type="text"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+        <h3>no hay datos aun</h3>
+        <Link to="/badges/new" className="badges__button">
+          Create New Badges
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="form-group">
+        <label className="form-group__label">Filter Badges</label>
+        <input
+          className="form-group__input"
+          type="text"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+      </div>
       <ul className="BadgesListItem__ul">
-        {this.props.Badges.map((badge) => {
+        {filteredBadges.map((badge) => {
           return (
             <li className="BadgesListItem" key={badge.id}>
               {/* PARA MANDAR A LA PAGINA EDIT CON SU PROPIO ID DE CADA PERSONA */}
@@ -43,8 +93,8 @@ class Badgelist extends React.Component {
           );
         })}
       </ul>
-    );
-  }
+    </>
+  );
 }
 
 export default Badgelist;
